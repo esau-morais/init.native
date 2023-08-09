d9426544af2ae49f9bf36d23822dc1f14bf875e3
esau-morais <esaumorais7@gmail.com>
2023-08-09 18:08:19 -0300

    feat: integrate with back-end

--- apps/otp/App.tsx
+++ apps/otp/App.tsx
@@ -1,14 +1,59 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import OTPInput, { OTPValue } from "./src/components/otp";
 import { useState } from "react";
 
 const App = () => {
-  const [otp, setOtp] = useState<OTPValue>([]);
+  const [otp, setOtp] = useState<OTPValue>(new Array(6).fill(""));
+  const [isVerifying, setIsVerifying] = useState(false);
+
+  const handleVerifyOTP = async () => {
+    try {
+      setIsVerifying(true);
+
+      const response = await fetch("http://localhost:3000/verify-otp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          // turn it dynamic
+          email: "me@emots.dev",
+          otp: otp.join(""),
+        }),
+      });
+
+      const data = await response.json();
+      console.info({ data });
+
+      if (response.ok) {
+        Alert.alert("Success", "OTP verified successfully.");
+        setOtp(Array(6).fill(""));
+      } else {
+        Alert.alert("Error", "Failed to verify OTP.");
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error", "Failed to verify OTP.");
+    } finally {
+      setIsVerifying(false);
+    }
+  };
 
   return (
     <View className="min-h-screen items-center justify-center">
-      <OTPInput length={6} value={otp} onChange={(value) => setOtp(value)} />
+      <OTPInput
+        length={6}
+        value={otp}
+        onChange={(value: OTPValue) => setOtp(value)}
+      />
+      <TouchableOpacity
+        className="mt-4 bg-black p-4 rounded-lg"
+        onPress={handleVerifyOTP}
+        disabled={isVerifying}
+      >
+        <Text className="text-white text-lg">Verify OTP</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
--- apps/otp/src/components/otp.tsx
+++ apps/otp/src/components/otp.tsx
@@ -1,7 +1,6 @@
 import { FC, useRef } from "react";
 import {
   View,
-  TouchableOpacity,
   TextInput,
   NativeSyntheticEvent,
   TextInputKeyPressEventData,
@@ -14,11 +13,10 @@ type OTPInputProps = {
   length: number;
   value: OTPValue;
   onChange: (otp: OTPValue) => void;
-} & TextInputProps;
+} & Omit<TextInputProps, "value" | "onChange">;
 
 const OTPInput: FC<OTPInputProps> = ({ length, value, onChange, ...props }) => {
   const inputRefs = useRef<TextInput[] | null>([]);
-  const currentInputEl = inputRefs.current;
 
   const handleValueChange = (text: string, index: number) => {
     const newValue = value.map((item, valueIndex) => {
@@ -32,15 +30,16 @@ const OTPInput: FC<OTPInputProps> = ({ length, value, onChange, ...props }) => {
   const handleInputChange = (value: string, index: number) => {
     handleValueChange(value, index);
 
-    if (!currentInputEl) return;
-    if (value.length !== 0) return currentInputEl[index + 1]?.focus();
+    if (!inputRefs.current) return;
 
-    return currentInputEl[index - 1]?.focus();
+    if (index < length - 1) inputRefs.current[index + 1]?.focus();
+    // If it's the last input, make sure to maintain focus
+    else inputRefs.current[index]?.focus();
   };
 
   const handleBackspace = (
     event: NativeSyntheticEvent<TextInputKeyPressEventData>,
-    index: number
+    index: number,
   ) => {
     if (event.nativeEvent.key === "Backspace") handleInputChange("", index);
   };
@@ -48,26 +47,22 @@ const OTPInput: FC<OTPInputProps> = ({ length, value, onChange, ...props }) => {
   return (
     <View className="flex-row justify-between space-x-2">
       {Array.from({ length }).map((_, index) => (
-        <TouchableOpacity
+        <TextInput
           key={index}
-          className="w-12 h-12 border rounded-lg items-center justify-center"
-        >
-          <TextInput
-            className="text-center"
-            ref={(ref) =>
-              ref && currentInputEl && !currentInputEl.includes(ref)
-                ? (inputRefs.current = [...currentInputEl, ref])
-                : null
-            }
-            maxLength={1}
-            keyboardType="decimal-pad"
-            contextMenuHidden
-            selectTextOnFocus
-            onChangeText={(value) => handleInputChange(value, index)}
-            onKeyPress={(value) => handleBackspace(value, index)}
-            {...props}
-          />
-        </TouchableOpacity>
+          className="w-12 h-12 border rounded-lg text-center"
+          ref={(ref) =>
+            ref && inputRefs.current && !inputRefs.current.includes(ref)
+              ? (inputRefs.current = [...inputRefs.current, ref])
+              : null
+          }
+          maxLength={1}
+          keyboardType="decimal-pad"
+          contextMenuHidden
+          selectTextOnFocus
+          onChangeText={(value) => handleInputChange(value, index)}
+          onKeyPress={(value) => handleBackspace(value, index)}
+          {...props}
+        />
       ))}
     </View>
   );
